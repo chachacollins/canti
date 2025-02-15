de4c26c15b8f83cb94c7d2ea9b371f424d5e7866
chachacollins <collinschacha@hotmail.com>
2025-02-15 21:45:10 +0300

    feat: small refactor

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,12 @@ const debug = @import("debug.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
+    defer {
+        const v = gpa.deinit();
+        if (v == .leak) {
+            std.debug.print("Memory leak detected\n", .{});
+        }
+    }
     var chunk = Chunk.init(allocator);
     defer chunk.deinit();
     try chunk.writeConstant(1.2, 123);
@@ -13,13 +18,3 @@ pub fn main() !void {
     try chunk.writeChunk(@intFromEnum(Chunk.Op_Code.OP_RETURN), 124);
     try debug.disassembleChunk(chunk, "Test Chunk");
 }
-
-// pub fn print(format: u8) !void{
-//     const stdout_file = std.io.getStdOut().writer();
-//     var bw = std.io.bufferedWriter(stdout_file);
-//     const stdout = bw.writer();
-//
-//     try stdout.print("Hello world\n", .{});
-//
-//     try bw.flush(); // don't forget to flush!
-// }
